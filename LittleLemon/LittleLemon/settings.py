1dab1cb0242fe5b42105a61918cf724068051276
suo3 <obiaproduction@gmail.com>
2024-12-02 20:39:22 -0500

    add models migration

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "restaurant",
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,27 @@ WSGI_APPLICATION = "LittleLemon.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+ """
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'littlelemon',
+        "USER": 'root',
+        "PASSWORD": '',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
